cf8f4822ed36c780d83f88dfe30668f79201a3c7
Florian Kinder <Florian.Kinder@fankserver.com>
2016-02-13 00:53:17 +0100

    Updated typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,18 @@
 import * as events from 'events';
 import * as net from 'net';
 
-declare interface LineInputStream extends events.EventEmitter {}
+declare interface LineInputStream extends events.EventEmitter {
+	addListener(type: string, listener: () => void);
+	removeListener(type: string, listener: () => void);
+	removeAllListeners(type: string);
+	pause();
+	resume();
+	destroy();
+	setEncoding(encoding: string);
+	setDelimiter(delimiter: string);
+	// get readable(): boolean;
+	// get
+}
 
 declare function constructor(socket: net.Socket): LineInputStream;
 
